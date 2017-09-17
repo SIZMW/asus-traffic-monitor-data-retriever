f9e8cfebd42acaf6ee87491f49284592df0871f1
Aditya Nivarthi <adityanivarthi19@gmail.com>
2017-09-17 01:13:49 -0700

    Initial commit.

---
+++ README.md
@@ -0,0 +1,58 @@
+ASUS Router Traffic Monitor Data Retriever
+=================================================
+
+## Description
+This program can be used to retrieve the daily traffic monitor data from an ASUS AC66U router.
+
+For those who want to monitor or track their daily and monthly internet usage, whether it be to identify unfamiliar data use or track their usage against ISP data caps, this program provides a means to gather, summarize, and extract their router traffic data and allow them to further visualize it as they see fit.
+
+Many routers with DD-WRT firmware offer the capability to see bandwidth usage, both live and in the past. Some provide basic views within the router administration web pages to see this information, but it isn't always easy to extract this and store it for further use. This program accesses the traffic monitor pages in the ASUS AC66U router administration, extracts the bandwidth usage data, and compiles and summarizes it in a Excel workbook that can then be used to generate charts and graphs for easy visualization.
+
+#### References
+
+* [cocaman/asus-traffic-fetcher](https://github.com/cocaman/asus-traffic-fetcher) was the initial inspiration to write this program, but with the ability to export to Excel workbooks.
+
+## Build
+This program requires:
+
+* [Python 2.7](https://www.python.org/download/releases/2.7/)
+* [Selenium](https://pypi.python.org/pypi/selenium/2.7.0)
+* [XlsxWriter](https://pypi.python.org/pypi/XlsxWriter)
+* [Docopt](https://pypi.python.org/pypi/docopt)
+* [ChromeDriver](https://sites.google.com/a/chromium.org/chromedriver/) for logging in and retrieving the data. Place the executable `chromedriver.exe` in the project folder.
+* An ASUS router to connect to (tested with AC66U)
+
+See the [requirements.txt](requirements.txt) for more information.
+
+## Execution
+
+#### Arguments
+To retrieve and store the usage data in an Excel workbook, run the program as follows:
+```
+python retrieve_traffic_data.py -o "path/to/output/Excel/workbook.xlsx"
+```
+
+The arguments are:
+
+* `output_file`: The output file where the daily usage and summary will be written.
+
+ASUS routers resolve to using the `http://router.asus.com` address from within the network, so there is no need to provide the IP address of the router. If your router exists at a different IP, edit the `ROUTER_URL` value in the script.
+
+You can run `python retrieve_traffic_data.py -h` for further help.
+
+#### Output
+An example of the daily usage output in the Excel workbook is shown below:
+
+|     Date     | Download | Upload |  Total  |
+|--------------|----------|--------|---------|
+|  2015-07-31  |   1.60   |  0.79  |  2.390  |
+|  2015-08-01  |   2.90   |  0.40  |   3.3   |
+|  2015-08-02  |   0.40   |  0.35  |  0.750  |
+|  2015-08-03  |   1.25   |  0.97  |  2.170  |
+
+An example of the monthly summary output in the Excel workbook is shown below:
+
+| Year | Month | Download | Upload |  Total  |
+|------|-------|----------|--------|---------|
+| 2015 |   7   |   1.60   |  0.79  |  2.390  |
+| 2015 |   8   |   4.55   |  1.72  |  6.270  |
---
+++ retrieve_traffic_data.py
@@ -0,0 +1,256 @@
+"""
+retrieve_traffic_data.py
+
+Retrieve the daily traffic data from an ASUS AC66U router and write the data and a summary into an Excel workbook.
+Router login is requested at the start of the program.
+
+Usage:
+    retrieve_traffic_data.py -o <output_file>
+    retrieve_traffic_data.py -h | --help
+    retrieve_traffic_data.py -v | --version
+
+Options:
+    -h --help           Show this help information.
+    -v --version        Show version number.
+    -i <ip>             The router IP address.
+    -o <output_file>    The output Excel workbook.
+"""
+
+from __future__ import print_function
+from datetime import datetime
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+from selenium.webdriver.support import expected_conditions as EC
+
+import csv
+import docopt
+import getpass
+import xlsxwriter
+
+ROUTER_URL = 'http://router.asus.com/'
+TRAFFIC_PAGE = 'Main_TrafficMonitor_daily.asp'
+LOGIN_PAGE = 'Main_Login.asp'
+CHROME_DRIVER_FILE = 'chromedriver.exe'
+
+DATE_COL = 'Date'
+DOWNLOAD_COL = 'Download'
+UPLOAD_COL = 'Upload'
+TOTAL_COL = 'Total'
+YEAR_COL = 'Year'
+MONTH_COL = 'Month'
+
+"""
+The DateTraffic class defines an entry in the traffic log for data usage on a specific day.
+"""
+class DateTraffic(object):
+    """
+    Constructs a DateTraffic instance.
+
+    Arguments:
+        dt: The date of the entry.
+        dl: The download usage.
+        ul: The upload usage.
+    """
+    def __init__(self, dt, dl, ul):
+        self.date = dt
+        self.dl = dl
+        self.ul = ul
+
+    """
+    Returns the total usage for this day.
+
+    Returns:
+        A float
+    """
+    @property
+    def total(self):
+        return self.dl + self.ul
+
+    """
+    Returns the string representation of this object.
+
+    Returns:
+        A string
+    """
+    def __repr__(self):
+        return '(date=%s, dl=%s, ul=%s, total=%s)' % (self.date, self.dl, self.ul, self.total)            
+
+"""
+Retrieves the daily usage data from the traffic monitor page in the ASUS router management web page.
+
+Arguments:
+    username: The user name for the administrator login.
+    password: The password for the administrator login.
+
+Returns:
+    An array of all the daily traffic usage entries.
+"""
+def retrieve_router_data(username, password):
+    # Set driver properties
+    capabilities = DesiredCapabilities.CHROME
+    capabilities['loggingPrefs'] = {'browser':'ALL'}
+    driver = webdriver.Chrome(CHROME_DRIVER_FILE, desired_capabilities=capabilities)
+    wait = WebDriverWait(driver, 120)
+    driver.implicitly_wait(2)
+    driver.set_window_size(960, 720)
+    
+    # Log in the router
+    driver.get(ROUTER_URL + LOGIN_PAGE)
+    wait.until(EC.element_to_be_clickable((By.NAME,'login_username')))
+    driver.find_element_by_name('login_username').click()
+    driver.find_element_by_name('login_username').send_keys(username)
+    driver.find_element_by_name('login_passwd').click()
+    driver.find_element_by_name('login_passwd').send_keys(password)
+    driver.find_element_by_css_selector('.button').click()
+    driver.implicitly_wait(2)
+
+    # Go to traffic page
+    driver.get(ROUTER_URL + TRAFFIC_PAGE)
+    driver.implicitly_wait(1)
+
+    # Get daily data
+    baseTable = driver.find_element_by_css_selector('.FormTable_NWM')
+    odd_table_rows = baseTable.find_elements_by_class_name('odd')
+    even_table_rows = baseTable.find_elements_by_class_name('even')
+
+    all_rows = []
+    all_rows.extend(odd_table_rows)
+    all_rows.extend(even_table_rows)
+
+    print('Pulled router daily traffic data.')
+    date_traffics = []
+
+    # Parse table rows
+    for row in all_rows:
+        items = row.find_elements_by_tag_name('td')
+
+        date = items[0].text
+        dl = float(items[1].text.split(' ')[0])
+        ul = float(items[2].text.split(' ')[0])
+
+        date_traffics.append(DateTraffic(date, dl, ul))
+
+    # Sort data
+    date_traffics.sort(key=lambda x: x.date, reverse=False)
+    driver.quit()
+
+    print('Sorted traffic table data.')
+    return date_traffics
+
+"""
+Writes the daily traffic usage entries and a summary into Excel worksheets.
+
+Arguments:
+    date_traffics: The array of all the daily traffic usage entries.
+    output_file: The output Excel workbook to write the data into.
+"""
+def write_workbook_output(date_traffics, output_file):
+    # Write daily data into Excel sheet
+    workbook = xlsxwriter.Workbook(output_file)
+    usage_wkst = workbook.add_worksheet('Router Usage')
+    usage_wkst.set_column(0, 3, 11)
+    date_format = workbook.add_format({'num_format': 'yyyy-mm-dd'})
+
+    row = 1
+    col = 0
+    for dt in date_traffics:
+        usage_wkst.write_datetime(row, col, format_date(dt.date), date_format)
+        usage_wkst.write_number(row, col + 1, dt.dl)
+        usage_wkst.write_number(row, col + 2, dt.ul)
+        usage_wkst.write_number(row, col + 3, dt.total)
+        row = row + 1
+
+    # Create table in worksheet
+    usage_wkst.add_table('A1:D{0}'.format(str(len(date_traffics) + 1)), {
+        'header_row': True,
+        'columns': [{
+            'header': DATE_COL
+        }, {
+            'header': DOWNLOAD_COL
+        }, {
+            'header': UPLOAD_COL
+        }, {
+            'header': TOTAL_COL
+        }] 
+    })
+
+    # Create dictionary of year and month entries
+    dt_summary = dict()    
+    for dt in date_traffics:
+        dt_datetime = format_date(dt.date)
+
+        dt_year = dt_datetime.year
+        dt_month = dt_datetime.month
+
+        dt_key = '{0}-{1}'.format(dt_year, dt_month)
+
+        if(dt_key in dt_summary):
+            dt_summary[dt_key][DOWNLOAD_COL] += dt.dl
+            dt_summary[dt_key][UPLOAD_COL] += dt.ul
+            dt_summary[dt_key][TOTAL_COL] += dt.total
+        else:
+            dt_summary[dt_key] = {
+                DOWNLOAD_COL: dt.dl,
+                UPLOAD_COL: dt.ul,
+                TOTAL_COL: dt.total
+            }
+
+    summ_wkst = workbook.add_worksheet('Router Summary')
+    summ_wkst.set_column(0, 4, 11)
+
+    # Write monthly summary data into Excel sheet
+    row = 1
+    col = 0
+    for dts in sorted(dt_summary.keys()):
+        dt_year = int(dts.split('-')[0])
+        dt_month = int(dts.split('-')[1])
+
+        summ_wkst.write_number(row, col, dt_year)
+        summ_wkst.write_number(row, col + 1, dt_month)
+
+        summ_wkst.write_number(row, col + 2, dt_summary[dts][DOWNLOAD_COL])
+        summ_wkst.write_number(row, col + 3, dt_summary[dts][UPLOAD_COL])
+        summ_wkst.write_number(row, col + 4, dt_summary[dts][TOTAL_COL])
+        row = row + 1
+
+    # Create table in worksheet
+    summ_wkst.add_table('A1:E{0}'.format(str(len(dt_summary.keys()) + 1)), {
+        'header_row': True,
+        'columns': [{
+            'header': YEAR_COL
+        }, {
+            'header': MONTH_COL
+        }, {
+            'header': DOWNLOAD_COL
+        }, {
+            'header': UPLOAD_COL
+        }, {
+            'header': TOTAL_COL
+        }] 
+    })
+
+    workbook.close()
+
+"""
+Returns the datetime object representation of the date string.
+
+Arguments:
+    date_str: The string form of the date (yyyy-mm-dd)
+
+Returns:
+    A datetime object
+"""
+def format_date(date_str):
+    return datetime.strptime(date_str, "%Y-%m-%d")
+
+
+if __name__ == '__main__':
+    arguments = docopt.docopt(__doc__, version='retrieve_traffic_data.py 0.1')
+    output_file = arguments['-o']
+    username = getpass.getpass('User Name: ')
+    password = getpass.getpass('Password: ')
+
+    write_workbook_output(retrieve_router_data(username, password), output_file)
+    print('Updated Excel workbook.')
